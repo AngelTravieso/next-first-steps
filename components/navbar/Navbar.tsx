40673d76ad615d02b19a518911ae5e1b6ec50f95
AngelTravieso <angeltraviesoc@gmail.com>
2024-05-02 14:55:02 -0400

    Server Components - Async await

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 // rafc -> functional component
 
+// 'use client' -> para marcar que se renderiza en el cliente (el navegador)
+// 'use server' -> por default los componentes se renderizan en el servidor
+
 import React from "react";
 
-export const Navbar = () => {
+export const Navbar = async () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
       <span>Home</span>
