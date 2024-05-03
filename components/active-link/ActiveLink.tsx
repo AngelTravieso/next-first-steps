46924e5377c7faeb6ade588e20f24f5d69fe0ad7
AngelTravieso <angeltraviesoc@gmail.com>
2024-05-03 10:55:27 -0400

    Explicacion prefetch

--- components/active-link/ActiveLink.tsx
+++ components/active-link/ActiveLink.tsx
@@ -14,8 +14,14 @@ export const ActiveLink = ({ path, text }: Props) => {
   // usePathname para ver la ruta actual en la que estamos
   const pathName = usePathname();
 
+  /**
+   * el prefetch carga la página antes de que entre al viewport
+   * por defecto está en true, se puede deshabilitar
+   */
+
   return (
     <Link
+      // prefetch={true}
       className={`${style.link} ${pathName === path && style["active-link"]}`}
       href={path}
     >
